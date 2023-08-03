961fcedfef6fe40697f977247029d9f944a07fcc
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-08-03 09:24:03 -0300

    add exercicio_oo

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const avatar = document.querySelector('.profile-avatar');
-    const name = document.querySelector('.profile-name');
-    const username = document.querySelector('.profile-username');
-    const repositories = document.querySelector('.repositories');
-    const followers = document.querySelector('.followers');
-    const following = document.querySelector('.following');
-    const profileLink = document.querySelector('.profile-link');
+function funcionario (nome, cargo, salario) {
+    this.nome = nome;
+    this.cargo = cargo;
+    this.salario = salario;
+    this.apresentar = function() {
+        console.log(`Olá, meu chamo ${this.nome}, é um prazer te conhecer.`);
+    }
+}
 
-    fetch('https://api.github.com/users/juliocjunior')
-    .then(function(res) {
-        return res.json()
-    })
-    .then (function(json) {
-        avatar.src = json.avatar_url;
-        name.innerHTML = json.name;
-        username.innerHTML = json.login;
-        repositories.innerHTML = json.public_repos;
-        followers.innerHTML = json.followers;
-        following.innerHTML = json.following;
-        profileLink.href = json.html_url;
-    })
-})
+function estagiario(nome,cargo,salario) {
+    funcionario.call(this, nome, cargo, salario);
+
+    this.cargo = 'Estágiario';
+    this.salario = 1000;
+
+    this.pedirAjuda = function() {
+        console.log(`${this.nome} diz: ei, você pode me ajudar pfv?`)
+    }
+}
+
+function gerente(nome, cargo, salario) {
+    funcionario.call(this, nome, cargo, salario);
+
+    this.cargo = 'Gerente';
+    this.salario = 10000;
+
+    this.vaiTrabalhar = function () {
+        console.log(`${this.nome} diz: Parem com isso e vão trabalhar!`);
+    }
+}
+
+const joao = new estagiario('João');
+const lucas = new estagiario('Lucas');
+const maria = new gerente('Maria');
+
+joao.pedirAjuda();
+maria.vaiTrabalhar();
