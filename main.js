0543f19536c8645a71801f54489c66aed56628b1
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-08-03 16:17:49 -0300

    add exercicio_es6

--- main.js
+++ main.js
@@ -1,57 +1,16 @@
-function funcionario (nome, cargo, salario) {
-    this.nome = nome;
-    this.cargo = cargo;
-    this.salario = salario;
-    this.apresentar = function() {
-        console.log(`Olá, meu chamo ${this.nome}, é um prazer te conhecer.`);
-    }
+const alunos = [
+    { nome: 'João', curso: 'Backend', nota: 7.5 },
+    { nome: 'Maria', curso: 'Frontend', nota: 5.0 },
+    { nome: 'Pedro', curso: 'Backend', nota: 8.0 },
+    { nome: 'Ana', curso: 'Mobile', nota: 6.5 },
+    { nome: 'Lucas', curso: 'Backend', nota: 6.0 }
+];
+
+function alunosAcimaDe6(alunos) {
+    return alunos.filter(function(aluno) {
+        return aluno.nota >= 6.0;
+    });
 }
 
-<<<<<<< HEAD
-function estagiario(nome,cargo,salario) {
-    funcionario.call(this, nome, cargo, salario);
-
-    this.cargo = 'Estágiario';
-    this.salario = 1000;
-
-    this.pedirAjuda = function() {
-        console.log(`${this.nome} diz: ei, você pode me ajudar pfv?`)
-    }
-}
-
-function gerente(nome, cargo, salario) {
-    funcionario.call(this, nome, cargo, salario);
-
-    this.cargo = 'Gerente';
-    this.salario = 10000;
-
-    this.vaiTrabalhar = function () {
-        console.log(`${this.nome} diz: Parem com isso e vão trabalhar!`);
-    }
-}
-
-const joao = new estagiario('João');
-const lucas = new estagiario('Lucas');
-const maria = new gerente('Maria');
-
-joao.pedirAjuda();
-maria.vaiTrabalhar();
-=======
-    fetch('https://api.github.com/users/juliocjunior')
-    .then(function(res) {
-        return res.json()
-    })
-    .then (function(json) {
-        avatar.src = json.avatar_url;
-        name.innerHTML = json.name;
-        username.innerHTML = json.login;
-        repositories.innerHTML = json.public_repos;
-        followers.innerHTML = json.followers;
-        following.innerHTML = json.following;
-        profileLink.href = json.html_url;
-    })
-    .catch (function(erro) {
-        alert('Ocorreu um erro ao buscar as informações, tente novamente mais tarde')
-    })
-})
->>>>>>> 8969ed3e2bdad521c45377f85d33e9db2356789a
+const alunosAprovados = alunosAcimaDe6(alunos);
+console.log(alunosAprovados);
