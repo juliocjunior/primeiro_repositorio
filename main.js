1f41f92e40574fbaaed019c3bff4c0c7925c1344
Julio Cesar Vieira Alves Junior <juliocesarjuniorjja@hotmail.com>
2023-08-03 16:21:02 -0300

    tirando a propriedade curso de alunos

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const alunos = [
-    { nome: 'João', curso: 'Backend', nota: 7.5 },
-    { nome: 'Maria', curso: 'Frontend', nota: 5.0 },
-    { nome: 'Pedro', curso: 'Backend', nota: 8.0 },
-    { nome: 'Ana', curso: 'Mobile', nota: 6.5 },
-    { nome: 'Lucas', curso: 'Backend', nota: 6.0 }
+    { nome: 'João', nota: 7.5 },
+    { nome: 'Maria', nota: 5.0 },
+    { nome: 'Pedro', nota: 8.0 },
+    { nome: 'Ana', nota: 6.5 },
+    { nome: 'Lucas', nota: 6.0 }
 ];
 
 function alunosAcimaDe6(alunos) {
