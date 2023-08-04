81cd0f31d4b79c12f7571f6cb3ea3e32419d3654
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-08-04 17:36:59 -0300

    add exercicio_ts

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(a, b) {
+    return a * b;
+}
+console.log(multiplica(2, 3));
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
+console.log(saudacao('Julio'));
